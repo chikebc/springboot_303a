e14cea4e41492acfc1540b5851d8683330d44db8
chikebc <chikecb@outlook.com>
2020-03-25 21:23:42 -0400

    Initial commit

---
+++ src/main/java/com/springboot_303a/springboot_303a/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.springboot_303a.springboot_303a;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
---
+++ src/main/java/com/springboot_303a/springboot_303a/Springboot303aApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_303a.springboot_303a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot303aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot303aApplication.class, args);
+    }
+
+}
